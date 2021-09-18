e8167e7c533d3bacdc41ca520f930e378c64e811
vitalydok <89067055+vitalydok@users.noreply.github.com>
2021-09-18 22:01:45 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const board = document.querySelector('#board')
+const colors = ['#264e2c', '#55761a', '#9db33e', '#dfe49b', '#f6f7cd']
+const SQUARSE_NUMBER = 1505
+
+for (let i = 0; i < SQUARSE_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
